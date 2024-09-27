e60a6e64781a752fcfae6ede2f73d4c3e82a68d3
Unlocksun <3307223445@qq.com>
2024-09-27 11:37:57 +0800

    - 添加注释

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -13,6 +13,7 @@ func main() {
 	if client, err := service.Dial("tcp", addr); err != nil {
 		log.Fatal("dial failed:", err)
 	} else {
+		// client会有routine负责数据接收. 主线程进行调用即可
 		defer client.Close()
 		var wg sync.WaitGroup
 		for i := 0; i < 5; i++ {
